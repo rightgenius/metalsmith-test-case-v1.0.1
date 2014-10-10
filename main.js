14d81d680305fa626f387a707e86e5604c3c6c65
nius <rightgenius@gmail.com>
2014-10-10 13:09:41 +0800

    make the case simple

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-var Metalsmith = require('metalsmith'),
-	markdown = require('metalsmith-markdown'),
-	permalinks = require('metalsmith-permalinks'),
-	templates  = require('metalsmith-templates'),
-	watch = require('metalsmith-watch');
+var Metalsmith = require('metalsmith');
 
 var metal = Metalsmith(__dirname)
-    .use(markdown())
-    .use(templates({
-    	'engine':'swig',
-    	'directory':'templates'
-    }))
-    .use(permalinks({
-    	'pattern': ':template/:type'
-    }))
     .source('./src')
     .destination('./build')
-    .use(watch())
     .build();
